6ddc17bd741be7b045f86baf3c788589c8ab4b82
IgorMendes <igorsmendes2803@gmail.com>
2020-05-31 14:59:27 -0300

    Adicionando Tab3 e pages
    Criando a tab de dessenhos e paginas para 2 filmes, 2 series e 2 desenhos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,30 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'bloodshot',
+    loadChildren: () => import('./filmes/bloodshot/bloodshot.module').then( m => m.BloodshotPageModule)
+  },
+  {
+    path: 'scooby',
+    loadChildren: () => import('./filmes/scooby/scooby.module').then( m => m.ScoobyPageModule)
+  },
+  {
+    path: 'flash',
+    loadChildren: () => import('./series/flash/flash.module').then( m => m.FlashPageModule)
+  },
+  {
+    path: 'game-of-thrones',
+    loadChildren: () => import('./series/game-of-thrones/game-of-thrones.module').then( m => m.GameOfThronesPageModule)
+  },
+  {
+    path: 'rick-and-morty',
+    loadChildren: () => import('./desenhos/rick-and-morty/rick-and-morty.module').then( m => m.RickAndMortyPageModule)
+  },
+  {
+    path: 'gravity-falls',
+    loadChildren: () => import('./desenhos/gravity-falls/gravity-falls.module').then( m => m.GravityFallsPageModule)
   }
 ];
 @NgModule({
